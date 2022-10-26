bed38c65db7e6d113eb7f3cdf2cf87e1cff25665
RohitSrinivasRG <rohit@mindgrovetech.in>
2022-10-26 06:14:41 +0000

    added code to measure var of signals

--- plan/output_processing/readleaks.py
+++ plan/output_processing/readleaks.py
@@ -1,14 +1,12 @@
 #------------------
-# from distutils.log import info
-# from imp import init_frozen
-# from distutils.log import info
 import os
 import re
-# from selectors import EpollSelector
+from tqdm import tqdm
+import numpy as np
 #------------------
 
 
-filePath = "/home/mindgrove/data/rohit/c-class-work/power-side-channel-analysis/plan/results/trial/cclass_add"
+filePath = "/home/mindgrove/data/rohit/c-class-test/power-side-channel-analysis/plan/results/trial/c_class"
 
 count = 0
 # Iterate directory
@@ -21,9 +19,11 @@ print('File count:', count)
 wf = open('final_leaks.txt','w')
 
 l={}
-
-for i in range(0,(count)):
-    with open (filePath+f"/leaks{i}"+".txt","r") as f:
+rfiles=os.listdir(filePath)
+# for i in range(0,(count)):
+    # with open (filePath+f"/leaks{i}"+".txt","r") as f:
+for i in tqdm(rfiles,desc="Processing Leak Files",total=len(rfiles)):
+    with open(filePath+f"/{i}") as f:
         # print("i",i)
         data=f.read().split('\n')
         for line in data:
@@ -42,14 +42,28 @@ for i in range(0,(count)):
                     l[info['signame']]=[info['val']]
 # print(l)
 
-# writing only the max value from all cycles
-for data in l:
-    print(data)
-    print(l[data])
-    wf.write(str(max(l[data]))+"\t\t"+data+"\n")
+# for data in tqdm(l,desc="Printing Leaks",total=len(l)):
+    
+#     #To print only the max value of the signal
+#     # wf.write(str(max(l[data]))+"\t\t"+data+"\n")
 
-    # wf.write(f"{l[data]:<10}\t\t{data:>20}\n")
+#     # Print all the values in array
+#     wf.write(f"{l[data]}\t\t{data:>20}\n")
 
 # print(l)
-print("completed ")
+
+# To print the variance of signals
+l_arr={}
+for data in l:
+    fp_val=[float(x) for x in (l[data])]
+    #creating numpy array
+    np_arr=np.array(fp_val)
+    l_arr[data]=np.var(np_arr)
+
+for data in tqdm(l_arr,desc="Printing Variance of signals",total=len(l)):
+    
+    #To print only the var value of the signal
+    wf.write(str(l_arr[data])+"\t\t"+f"{data:>50}"+"\n")
+
+
 #------------------------------------------------------------------------------------------------------#
