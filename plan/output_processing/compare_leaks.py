f5e033ac0765aff22ee1bc0343008c8958f9ed5a
RohitSrinivasRG <rohit@mindgrovetech.in>
2022-11-17 05:45:31 +0000

    seperate report file for each module(dcache,fillbuffer,registerfile)

--- plan/output_processing/compare_leaks.py
+++ plan/output_processing/compare_leaks.py
@@ -8,6 +8,12 @@ noobs_path="/home/mindgrove/data/rohit/c-class-test/power-side-channel-analysis/
 obs_path="/home/mindgrove/data/rohit/c-class-work/power-side-channel-analysis/plan/output_processing/final_leaks.txt"
 #File Path for the comparison report
 compare_report="/home/mindgrove/data/rohit/c-class-work/power-side-channel-analysis/plan/output_processing/comparison_report.txt"
+#Separate file for separate modules
+dcache_file="/home/mindgrove/data/rohit/c-class-work/power-side-channel-analysis/plan/output_processing/dcache_sig.txt"
+fillbuffer_file="/home/mindgrove/data/rohit/c-class-work/power-side-channel-analysis/plan/output_processing/fillbuffer_sig.txt"
+registerfile_file="/home/mindgrove/data/rohit/c-class-work/power-side-channel-analysis/plan/output_processing/registerfile_sig.txt"
+# dcache_file="/home/mindgrove/data/rohit/c-class-work/power-side-channel-analysis/plan/output_processing/dcache_sig.txt"
+
 
 #Opening both result files in read mode
 f_obs=open(obs_path,"r")
@@ -125,33 +131,72 @@ for sig in ls_obs:
     wf_comp.write("OBS = "+dict_obs[sig]+"  Non-OBS = "+dict_noobs[sig]+"  "+sig+"\n")        
 wf_comp.write("-------------------------------------------------\n")
 wf_comp.write("-------------------------------------------------\n")
+
+#Putting the signal is seperate files
 #Printing the signals present in dcache
-wf_comp.write("Signals present in dcache\n")
-wf_comp.write("All dcache signals\n")
-sig_dcache=0
-for sig in dict_obs:
-    if(re.search("TOP.mkTbSoc.soc.ccore.dmem.dcache.",sig)):
-        if(float(dict_obs[sig])>0.6):
+with open(dcache_file,"w") as wf_dcache:
+    wf_dcache.write("Signals present in dcache\n")
+    wf_dcache.write("All dcache signals\n")
+    sig_dcache=0
+    dcache_obs_noobs=[]
+    dcache_obs=[]
+    for sig in dict_obs:
+        if(re.search("TOP.mkTbSoc.soc.ccore.dmem.dcache.",sig)):
+            # if(float(dict_obs[sig])>0.6):
             sig_dcache+=1
             if sig in dict_noobs:
-                wf_comp.write("OBS = "+dict_obs[sig]+"  Non-OBS = "+dict_noobs[sig]+"  "+sig+"\n")        
+                dcache_obs_noobs.append("OBS = "+dict_obs[sig]+"  Non-OBS = "+dict_noobs[sig]+"  "+sig+"\n")        
             else:
-                wf_comp.write("OBS = "+dict_obs[sig]+"  "+sig+"\n")        
-wf_comp.write("Number of leaked signals in dcache = "+str(sig_dcache)+"\n")
-wf_comp.write("-------------------------------------------------\n")
-#Printing the signals present in stage2 registerfile
-wf_comp.write("Signals present in registerfile\n")
-wf_comp.write("All registerfile signals\n")
-sig_regfile=0
-for sig in dict_obs:
-    if(re.search("TOP.mkTbSoc.soc.ccore.riscv.stage2.registerfile.",sig)):
-        # if(float(dict_obs[sig])>0.6):
-        sig_regfile+=1
-        if sig in dict_noobs:
-            wf_comp.write("OBS = "+dict_obs[sig]+"  Non-OBS = "+dict_noobs[sig]+"  "+sig+"\n")        
-        else:
-            wf_comp.write("OBS = "+dict_obs[sig]+"  "+sig+"\n")        
-wf_comp.write("Number of leaked signals in registerfile = "+str(sig_regfile)+"\n")
+                dcache_obs.append("OBS = "+dict_obs[sig]+"  "+sig+"\n") 
+    wf_dcache.write("Number of leaked signals in dcache = "+str(sig_dcache)+"\n")               
+    for i in dcache_obs_noobs:
+        wf_dcache.write(f"{i}")
+    for i in dcache_obs:
+        wf_dcache.write(f"{i}")
+    wf_dcache.write("-------------------------------------------------\n")
+#Printing the signals present in fillbuffer
+with open(fillbuffer_file,"w") as wf_fb:
+    wf_fb.write("Signals present in fillbuffer\n")
+    wf_fb.write("All fillbuffer signals\n")
+    sig_fb=0
+    fb_obs_noobs=[]
+    fb_obs=[]
+    for sig in dict_obs:
+        if(re.search("TOP.mkTbSoc.soc.ccore.dmem.dcache.m_fillbuffer.",sig)):
+            # if(float(dict_obs[sig])>0.6):
+            sig_fb+=1
+            if sig in dict_noobs:
+                fb_obs_noobs.append("OBS = "+dict_obs[sig]+"  Non-OBS = "+dict_noobs[sig]+"  "+sig+"\n")        
+            else:
+                fb_obs.append("OBS = "+dict_obs[sig]+"  "+sig+"\n") 
+    wf_fb.write("Number of leaked signals in fillbuffer = "+str(sig_fb)+"\n")               
+    for i in dcache_obs_noobs:
+        wf_fb.write(f"{i}")
+    for i in dcache_obs:
+        wf_fb.write(f"{i}")
+    wf_fb.write("-------------------------------------------------\n")
+
+# Printing signals in registerfile
+with open(registerfile_file,"w") as wf_regfile:
+    wf_regfile.write("Signals present in registerfile\n")
+    wf_regfile.write("All registerfile signals\n")
+    sig_regfile=0
+    rf_obs_noobs=[]
+    rf_obs=[]
+    for sig in dict_obs:
+        if(re.search("TOP.mkTbSoc.soc.ccore.riscv.stage2.registerfile.",sig)):
+            # if(float(dict_obs[sig])>0.6):
+            sig_regfile+=1
+            if sig in dict_noobs:
+                rf_obs_noobs.append("OBS = "+dict_obs[sig]+"  Non-OBS = "+dict_noobs[sig]+"  "+sig+"\n")        
+            else:
+                rf_obs.append("OBS = "+dict_obs[sig]+"  "+sig+"\n")       
+    for i in rf_obs_noobs:
+        wf_regfile.write(f"{i}")
+    for i in rf_obs:
+        wf_regfile.write(f"{i}")
+    wf_regfile.write("Number of leaked signals in registerfile = "+str(sig_regfile)+"\n")
+    wf_regfile.write("============================================================================================================\n")
 wf_comp.write("============================================================================================================\n")
 
 
